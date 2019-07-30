4b65f4c30e383d4adb1694b385f8c6370ef07d32
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-07-29 23:46:22 -0700

    (WIP) Replaces placeholder data with API data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
-let gameList = [
-    {"id": 148228, "name":"Splendor", "minplayers":2, "maxplayers":4, "minplaytime":30, "maxplaytime":30},
-    {"id": 199478, "name":"Flamme Rouge", "minplayers":2, "maxplayers":4, "minplaytime":30, "maxplaytime":45},
-    {"id": 169786, "name":"Scythe", "minplayers":1, "maxplayers":5, "minplaytime":90, "maxplaytime":115},
-    {"id": 37904, "name":"Formula D", "minplayers":2, "maxplayers":10, "minplaytime":60, "maxplaytime":60},
-    {"id": 180263, "name":"The 7th Continent", "minplayers":1, "maxplayers":4, "minplaytime":5, "maxplaytime":1000}
+// let gameListStatic = [
+//     {"id": 148228, "name":"Splendor", "minplayers":2, "maxplayers":4, "minplaytime":30, "maxplaytime":30},
+//     {"id": 199478, "name":"Flamme Rouge", "minplayers":2, "maxplayers":4, "minplaytime":30, "maxplaytime":45},
+//     {"id": 169786, "name":"Scythe", "minplayers":1, "maxplayers":5, "minplaytime":90, "maxplaytime":115},
+//     {"id": 37904, "name":"Formula D", "minplayers":2, "maxplayers":10, "minplaytime":60, "maxplaytime":60},
+//     {"id": 180263, "name":"The 7th Continent", "minplayers":1, "maxplayers":4, "minplaytime":5, "maxplaytime":1000}
+// ]
+
+let gameListApi = [
+    {"id": 0, "name":"(no API data)", "minplayers":0, "maxplayers":0, "minplaytime":0, "maxplaytime":0}
 ]
 
 const Game = ({id=-1, name="No Name Provided", minplayers=-1, maxplayers=-1, minplaytime=-1, maxplaytime=-1}) => {
@@ -23,13 +27,79 @@ const Game = ({id=-1, name="No Name Provided", minplayers=-1, maxplayers=-1, min
     )
 }
 
-// conditional rendering
-class GameBag extends React.Component {
+function extractFromXml(str) {
 
-    static defaultProps = {
-        games: [
-            {"id": -1, "name": "<DEFAULT>", "minplayers": -1, "maxplayers": -1, "minplaytime": -1, "maxplaytime": -1}
-        ]
+    let game = {}
+    let responseDoc = new DOMParser().parseFromString(str, 'application/xml')
+    let gamesHtmlCollection = responseDoc.getElementsByTagName("item")
+    game['id'] = gamesHtmlCollection[0].id
+
+    gamesHtmlCollection[0].childNodes.forEach(
+        function (node) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if ( (node.tagName === "name") && (node.getAttribute("type") === "primary") ) {
+                    game['name'] = node.getAttribute("value")
+                }
+                if (node.tagName === "yearpublished") {
+                    game['yearpublished'] = node.getAttribute("value")
+                }
+                if (node.tagName === "minplaytime") {
+                    game['minplaytime'] = node.getAttribute("value")
+                }
+                if (node.tagName === "maxplaytime") {
+                    game['maxplaytime'] = node.getAttribute("value")
+                }
+                if ( (node.tagName === "link")
+                    && (node.getAttribute("type") === "boardgamecategory") ) {
+                    if (game.hasOwnProperty('boardgamecategory')) {
+                        game['boardgamecategory'].push(node.getAttribute("value"))
+                    } else {
+                        game['boardgamecategory'] = new Array(node.getAttribute("value"))
+                    }
+                }
+            }
+        }
+    )
+    //console.log('game: ', game)
+}
+
+let urls = [
+    'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=31260',
+    'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=5',
+    'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=205637'
+
+    // 'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=148228',
+    // 'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=199478',
+    // 'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=169786',
+    // 'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=37904',
+    // 'https://boardgamegeek.com/xmlapi2/thing?type=boardgame&id=180263'
+]
+
+class GameShelf extends React.Component {
+
+    state = { 
+        gameInfo: [],
+        loading: true
+    }
+
+
+    async componentDidMount() {
+        //console.log("The component is now mounted")
+
+        const gameInfoJsonArray = await Promise.all(
+            urls.map(
+                url =>
+                    fetch(url)
+                        .then(response => response.text())
+                        .then(text => extractFromXml(text))
+                    ))
+
+        this.setState({ gameInfo: gameInfoJsonArray, loading: false })
+
+    }
+
+    componentDidUpdate() {
+        //console.log("The component just updated")
     }
 
     render() {
@@ -40,7 +110,7 @@ class GameBag extends React.Component {
                     (game, i) => 
                         <Game
                             key={i}
-                            id={game.title} 
+                            id={game.id} 
                             name={game.name} 
                             minplayers={game.minplayers} 
                             maxplayers={game.maxplayers} 
@@ -53,6 +123,6 @@ class GameBag extends React.Component {
 }
 
 render(
-    <GameBag games={gameList}/>,
+    <GameShelf games={gameListApi}/>,
     document.getElementById('root')
 )

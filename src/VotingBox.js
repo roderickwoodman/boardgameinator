cc92e313d62552fb9d5f30513e1815a241a84098
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-08-12 16:14:36 -0700

    Adds conditional rendering of sidebar components

--- src/VotingBox.js
+++ src/VotingBox.js
@@ -6,60 +6,80 @@ export class VotingBox extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            voteSection: 'players',
+            votingOn: 'players',
         }
-        this.handleSortChange = this.handleSortChange.bind(this)
+        this.handleSectionChange = this.handleSectionChange.bind(this)
     }
 
-    handleSortChange(event) {
+    handleSectionChange(event) {
         this.setState({
-            voteSection: event.target.value
+            votingOn: event.target.value
         })
-        console.log('(FIXME, sidebar should only display the ', event.target.value, 'content)')
+    }
+
+    renderSection() {
+        if (this.state.votingOn === 'players') {
+            return(
+                <VotingSection 
+                    type='players'
+                    id='supported-players'
+                    title='PLAYERS:'
+                    counts={this.props.playercounts}
+                    thumbs={this.props.thumbs['players']}
+                    onnewvote={this.props.onnewvote}
+                    onclearsectionvotes={this.props.onclearsectionvotes} />
+            )
+        }
+        if (this.state.votingOn === 'categories') {
+            return(
+                <VotingSection 
+                    type='category'
+                    id='category-counts'
+                    title='CATEGORY:'
+                    counts={this.props.categorycounts}
+                    thumbs={this.props.thumbs['category']}
+                    onnewvote={this.props.onnewvote}
+                    onclearsectionvotes={this.props.onclearsectionvotes} />
+            )
+        }
+        if (this.state.votingOn === 'mechanics') {
+            return(
+                <VotingSection 
+                    type='mechanic'
+                    id='mechanic-counts'
+                    title='MECHANIC:'
+                    counts={this.props.mechaniccounts}
+                    thumbs={this.props.thumbs['mechanic']}
+                    onnewvote={this.props.onnewvote}
+                    onclearsectionvotes={this.props.onclearsectionvotes} />
+            )
+        }
     }
 
     render() {
         return (
             <React.Fragment>
 
-            <div id="vote-sections">
+            <div id="votesection-selector">
                 <div>
                     <label>
-                        <input type='radio' key='players' id='players' name='votesection' checked={this.state.voteSection==='players'} value='players' onChange={this.handleSortChange} /> 
+                        <input type='radio' key='players' id='players' name='votesection' checked={this.state.votingOn==='players'} value='players' onChange={this.handleSectionChange} /> 
                         Players</label>
                     <label>
-                        <input type='radio' key='categories' id='categories' name='votesection' checked={this.state.voteSection==='categories'} value='categories' onChange={this.handleSortChange} /> 
+                        <input type='radio' key='categories' id='categories' name='votesection' checked={this.state.votingOn==='categories'} value='categories' onChange={this.handleSectionChange} /> 
                         Categories</label>
                     <label>
-                        <input type='radio' key='mechanics' id='mechanics' name='votesection' checked={this.state.voteSection==='mechanics'} value='mechanics' onChange={this.handleSortChange} /> 
+                        <input type='radio' key='mechanics' id='mechanics' name='votesection' checked={this.state.votingOn==='mechanics'} value='mechanics' onChange={this.handleSectionChange} /> 
                         Mechanics</label>
                 </div>
             </div>
-            <button data-attrtype="all" onClick={this.props.onclearsectionvotes}>CLEAR ALL</button>
-            <VotingSection
-                type='players'
-                id='supported-players'
-                title='PLAYERS:' 
-                counts={this.props.playercounts}
-                thumbs={this.props.thumbs['players']} 
-                onnewvote={this.props.onnewvote}
-                onclearsectionvotes={this.props.onclearsectionvotes}/>
-            <VotingSection
-                type='category'
-                id='category-counts'
-                title='CATEGORY:' 
-                counts={this.props.categorycounts}
-                thumbs={this.props.thumbs['category']} 
-                onnewvote={this.props.onnewvote}
-                onclearsectionvotes={this.props.onclearsectionvotes}/>
-            <VotingSection
-                type='mechanic'
-                id='mechanic-counts'
-                title='MECHANIC:' 
-                counts={this.props.mechaniccounts}
-                thumbs={this.props.thumbs['mechanic']} 
-                onnewvote={this.props.onnewvote}
-                onclearsectionvotes={this.props.onclearsectionvotes}/>
+
+            <button data-attrtype="all" onClick={this.props.onclearsectionvotes}>CLEAR ALL VOTES</button>
+
+            <div id="voting-section">
+                { this.renderSection() }
+            </div>
+
             </React.Fragment>
         )
     }
--- src/VotingSection.js
+++ src/VotingSection.js
@@ -6,7 +6,7 @@ export class VotingSection extends React.Component {
     render() {
         return (
             <ul id={this.props.id}>
-                <li><b>{this.props.title}</b><button data-attrtype={this.props.type} onClick={this.props.onclearsectionvotes}>clear all</button></li>
+                <li><b>{this.props.title}</b><button data-attrtype={this.props.type} onClick={this.props.onclearsectionvotes}>clear section votes</button></li>
                 {this.props.counts.map((key, index) => {
                     return <VotableElement 
                         key={key.attrName}

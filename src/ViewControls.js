869f8cbdcc60bbe08af26aa17ea4518532d06e44
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-09-09 10:18:56 -0700

    Styles the remaining instruction header sections for consistency

--- src/ViewControls.js
+++ src/ViewControls.js
@@ -7,22 +7,26 @@ export const ViewControls = (props) => {
     return (
         <React.Fragment>
         <div id="gamesorting-controls">
-            <span className='instructions'>
-                <span className='circledNumber'>&#9314;</span>Sort the results.
+            <span className="instructions">
+                <span className="leftGroup">
+                    <span className="circledNumber">&#9314;</span>Sort the results.
+                </span>
             </span>
             <label>
-                <input type='radio' key='maxvotes' id='maxvotes' name='sortorder' checked={props.sortby==='maxvotes'} value='maxvotes' onChange={props.onsortchange} /> 
+                <input type="radio" key="maxvotes" id="maxvotes" name="sortorder" checked={props.sortby==="maxvotes"} value="maxvotes" onChange={props.onsortchange} /> 
                 sort by thumbsup votes</label>
             <label>
-                <input type='radio' key='maxplaytime' id='maxplaytime' name='sortorder' checked={props.sortby==='maxplaytime'} value='maxplaytime' onChange={props.onsortchange} /> 
+                <input type="radio" key="maxplaytime" id="maxplaytime" name="sortorder" checked={props.sortby==="maxplaytime"} value="maxplaytime" onChange={props.onsortchange} /> 
                 sort by playtime</label>
             <label>
-                <input type='radio' key='maxplayers' id='maxplayers' name='sortorder' checked={props.sortby==='maxplayers'} value='maxplayers' onChange={props.onsortchange} /> 
+                <input type="radio" key="maxplayers" id="maxplayers" name="sortorder" checked={props.sortby==="maxplayers"} value="maxplayers" onChange={props.onsortchange} /> 
                 sort by player count</label>
         </div>
         <div id="gamefiltering-controls">
-            <span className='instructions'>
-                <span className='circledNumber'>&#9315;</span>Filter the results.
+            <span className="instructions">
+                <span className="leftGroup">
+                    <span className="circledNumber">&#9315;</span>Filter the results.
+                </span>
             </span>
             <label>
                 <input type="checkbox" id="filterplayercount" checked={props.filterplayercount} onChange={props.onfilterchange} />

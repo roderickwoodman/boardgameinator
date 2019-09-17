22bbbb11922e24b4d0b9f37d2a63d0b92f5703ea
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-09-17 09:31:39 -0700

    Adds status message to copy-to-clipboard action

--- src/AddedList.js
+++ src/AddedList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { AddedElement } from './AddedElement';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 
 
 export class AddedList extends React.Component {
@@ -16,6 +18,10 @@ export class AddedList extends React.Component {
     handleCopyToClipboard(event) {
         document.getElementById("games-clipboard").select()
         document.execCommand("copy")
+        let messages = []
+        let numgames = document.getElementById("games-clipboard").value.split("\n").length - 1
+        messages.push("Copied " + numgames + " games to clipboard")
+        this.setState({ statusMessages: messages })
     }
 
     render() {
@@ -24,7 +30,6 @@ export class AddedList extends React.Component {
             clipboardValue += game.name.concat((game.hasOwnProperty("nameisunique") && game["nameisunique"] === false) ? game["disambiguation"] : "").concat("\n")
         })
         let inlineStyle = {
-            // display: 'none'
             position: "absolute",
             left: "-1000px",
             top: "-1000px"
@@ -56,6 +61,17 @@ export class AddedList extends React.Component {
                     <textarea id="games-clipboard" style={inlineStyle} defaultValue={clipboardValue}></textarea>
                 </form>
             </section>
+            <div className="status-messages">
+                { this.state.statusMessages
+                    .map(
+                        (message, i) => {
+                            return (message.toLowerCase().startsWith("error"))
+                            ? <p key={i} className="message error">{message}</p>
+                            : <p key={i} className="message"><FontAwesomeIcon icon={faLongArrowAltRight} /> {message}</p>
+                        }
+                    )
+                }
+            </div>
             </React.Fragment>
         )
     }

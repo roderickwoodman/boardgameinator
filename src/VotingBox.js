d6f33e19f96fa271dc1268cbafd2c2a9765492e6
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-09-24 21:05:09 -0700

    Adds animation to the segmented controls for voting

--- src/VotingBox.js
+++ src/VotingBox.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { VotingSection } from './VotingSection';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export class VotingBox extends React.Component {
 
@@ -59,53 +60,91 @@ export class VotingBox extends React.Component {
             </ul>
 
             <div id="voting-section">
-                {this.state.votingOn === 'players' && (
-                    <VotingSection 
-                        type='players'
-                        elementid='supported-players'
-                        title='PLAYERS:'
-                        counts={this.props.playercounts}
-                        thumbs={this.props.thumbs['players']}
-                        onnewvote={this.props.onnewvote}
-                        onclearsectionvotes={this.props.onclearsectionvotes} />
-                )}
-                {this.state.votingOn === 'weights' && (
-                    <VotingSection 
-                        type='weight'
-                        elementid='weight-counts'
-                        title='WEIGHT:'
-                        counts={this.props.weightcounts}
-                        thumbs={this.props.thumbs['weight']}
-                        onnewvote={this.props.onnewvote}
-                        onclearsectionvotes={this.props.onclearsectionvotes} />
-                )}
-                {this.state.votingOn === 'categories' && (
-                    <VotingSection 
-                        type='category'
-                        elementid='category-counts'
-                        title='CATEGORY:'
-                        counts={this.props.categorycounts}
-                        thumbs={this.props.thumbs['category']}
-                        onnewvote={this.props.onnewvote}
-                        onclearsectionvotes={this.props.onclearsectionvotes} />
-                )}
-                {this.state.votingOn === 'mechanics' && (
-                    <VotingSection 
-                        type='mechanic'
-                        elementid='mechanic-counts'
-                        title='MECHANIC:'
-                        counts={this.props.mechaniccounts}
-                        thumbs={this.props.thumbs['mechanic']}
-                        onnewvote={this.props.onnewvote}
-                        onclearsectionvotes={this.props.onclearsectionvotes} />
-                )}
+                <TransitionGroup>
+                {this.state.votingOn === 'players' &&
+                    <CSSTransition
+                        key={0}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <VotingSection 
+                            type='players'
+                            elementid='supported-players'
+                            title='PLAYERS:'
+                            counts={this.props.playercounts}
+                            thumbs={this.props.thumbs['players']}
+                            onnewvote={this.props.onnewvote}
+                            onclearsectionvotes={this.props.onclearsectionvotes} 
+                        />
+                    </CSSTransition>
+                }
+                {this.state.votingOn === 'weights' &&
+                    <CSSTransition
+                        key={1}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <VotingSection 
+                            type='weight'
+                            elementid='weight-counts'
+                            title='WEIGHT:'
+                            counts={this.props.weightcounts}
+                            thumbs={this.props.thumbs['weight']}
+                            onnewvote={this.props.onnewvote}
+                            onclearsectionvotes={this.props.onclearsectionvotes} 
+                        />
+                    </CSSTransition>
+                }
+                {this.state.votingOn === 'categories' &&
+                    <CSSTransition
+                        key={2}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <VotingSection 
+                            type='category'
+                            elementid='category-counts'
+                            title='CATEGORY:'
+                            counts={this.props.categorycounts}
+                            thumbs={this.props.thumbs['category']}
+                            onnewvote={this.props.onnewvote}
+                            onclearsectionvotes={this.props.onclearsectionvotes} 
+                        />
+                    </CSSTransition>
+                }
+                {this.state.votingOn === 'mechanics' &&
+                    <CSSTransition
+                        key={3}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <VotingSection 
+                            type='mechanic'
+                            elementid='mechanic-counts'
+                            title='MECHANIC:'
+                            counts={this.props.mechaniccounts}
+                            thumbs={this.props.thumbs['mechanic']}
+                            onnewvote={this.props.onnewvote}
+                            onclearsectionvotes={this.props.onclearsectionvotes} 
+                        />
+                    </CSSTransition>
+                }
+                </TransitionGroup>
                 {this.props.playercounts.length === 0 
                 && this.props.weightcounts.length === 0 
                 && this.props.categorycounts.length === 0
                 && this.props.mechaniccounts.length === 0
-                && (
+                &&
                     this.emptyMessage()
-                )}
+                }
             </div>
 
             </React.Fragment>

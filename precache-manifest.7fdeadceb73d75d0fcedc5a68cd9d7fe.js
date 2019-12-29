bd19547609e34d815955c064badd45598c227a36
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-12-29 12:15:22 -0800

    Updates

---
+++ precache-manifest.7fdeadceb73d75d0fcedc5a68cd9d7fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2f0bfbe69df437ae671d8488d5e2dd",
+    "url": "/boardgameinator/index.html"
+  },
+  {
+    "revision": "2993559c165c3516c4e7",
+    "url": "/boardgameinator/static/css/main.48adc770.chunk.css"
+  },
+  {
+    "revision": "c7f5cdeb30e141f2dfbf",
+    "url": "/boardgameinator/static/js/2.54f859d8.chunk.js"
+  },
+  {
+    "revision": "2993559c165c3516c4e7",
+    "url": "/boardgameinator/static/js/main.b6fc4e23.chunk.js"
+  },
+  {
+    "revision": "670d3fa99576bb756c63",
+    "url": "/boardgameinator/static/js/runtime~main.ef3cdeed.js"
+  }
+]);

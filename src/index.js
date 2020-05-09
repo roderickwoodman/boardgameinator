a3d1e6284cf76c892d988e80a45d455ac8ce3ef0
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-05-09 14:30:01 -0700

    Refactors high level components to add routing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import { Boardgameinator } from './Boardgameinator'
 
 
-let gameListDefault = [
-    {
-        "id": 0, 
-        "name":"(no name info)",
-        "description":"(no description)",
-        "yearpublished":0,
-        "minplayers":0,
-        "maxplayers":0,
-        "minplaytime":0,
-        "maxplaytime":0,
-        "categories":null,
-        "mechanics":null
-    }
-]
-
-render(
-    <Boardgameinator defaultgames={gameListDefault}/>,
-    document.getElementById('root')
+const routing = (
+    <Router>
+        <div>
+            <Route path="/" component={Boardgameinator} />
+        </div>
+    </Router>
 )
+
+ReactDOM.render(routing, document.getElementById('root'))

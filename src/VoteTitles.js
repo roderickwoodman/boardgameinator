365527b4637679d56cd28051e413816cd653daaf
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-06-26 23:38:08 -0700

    Refactors the VoteTitles component to be an SFC

--- src/VoteTitles.js
+++ src/VoteTitles.js
@@ -2,39 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { VotingSection } from './VotingSection';
 
+export const VoteTitles = (props) => {
 
-export class VoteTitles extends React.Component {
+    let countsArray = []
+    props.allgames.forEach(function(game) {
+        let game_name = game.name.concat((game.hasOwnProperty("name_is_unique") && game["name_is_unique"] === false) ? game["disambiguation"] : "")
+        countsArray.push({'attrName': game_name, 'attrCount': 1})
+    })
 
-    constructor(props) {
-        super(props)
-        this.state = { 
-        }
-    }
-
-    render() {
-
-        let countsArray = []
-        this.props.allgames.forEach(function(game) {
-            let game_name = game.name.concat((game.hasOwnProperty("name_is_unique") && game["name_is_unique"] === false) ? game["disambiguation"] : "")
-            countsArray.push({'attrName': game_name, 'attrCount': 1})
-        })
-
-        return (
-            <React.Fragment>
-            <h4>Upvote board game titles:</h4>
-            <VotingSection 
-                type='title'
-                elementid='game-titles'
-                title='TITLES:'
-                counts={countsArray}
-                sectionthumbs={this.props.titlethumbs}
-                onnewvote={this.props.onnewvote}
-                alphabetize={true}
-                suppresslowcounts={true}
-            />
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+        <h4>Upvote board game titles:</h4>
+        <VotingSection 
+            type='title'
+            elementid='game-titles'
+            title='TITLES:'
+            counts={countsArray}
+            sectionthumbs={props.titlethumbs}
+            onnewvote={props.onnewvote}
+            alphabetize={true}
+            suppresslowcounts={true}
+        />
+        </React.Fragment>
+    )
 }
 
 VoteTitles.propTypes = {

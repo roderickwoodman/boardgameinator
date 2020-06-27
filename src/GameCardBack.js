190f6306ed5df761bfbbe124e1771f2b16ef504a
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-06-27 11:37:51 -0700

    Refactors the GameCardBack component

--- src/GameCardBack.js
+++ src/GameCardBack.js
@@ -7,8 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-
-function Description(props) {
+const Description = (props) => {
     const paragraphs = props.description.map ( (paragraph, idx) =>
         <p key={idx}>{paragraph}</p>
     )
@@ -23,7 +22,7 @@ Description.propTypes = {
     description: PropTypes.array.isRequired,
 }
 
-function Comments(props) {
+const Comments = (props) => {
     const comments = props.comments.map ( (comment, idx) =>
         <li key={idx}><b>{comment.author}: </b>{comment.comment}</li>
     )
@@ -38,7 +37,7 @@ Comments.propTypes = {
     comments: PropTypes.array.isRequired,
 }
 
-function Videos(props) {
+const Videos = (props) => {
     const videos = props.videos.map ( (video, idx) =>
         <li key={idx}><b>{video.author}: </b><a href={video.link} target="_blank" rel="noopener noreferrer">{video.title}</a></li>
     )
@@ -53,85 +52,82 @@ Videos.propTypes = {
     videos: PropTypes.array.isRequired,
 }
 
-export class GameCardBack extends React.Component {
+export const GameCardBack = (props) => {
 
-    render() {
+    const { id, name, yearpublished, description, inspectingsection, comments, videos, allthumbs, ontoggleinspection, oninspectionsectionchange, onnewvote, ondelete, thumbnail, thumbcounts } = this.props
+    return (
+        <React.Fragment>
+        <section className="gamecard-header">
+            <button className="fa fa-button" onClick={ (e) => ondelete(e, id) }><FontAwesomeIcon icon={faTrash}/></button>
+            <button className="fa fa-button inspect" onClick={ (e) => ontoggleinspection(e, id) }><FontAwesomeIcon icon={faInfoCircle}/></button>
+        </section>
+        <section className="gamecard-collapse-control">
+            <button className="fa fa-button inspect" onClick={ (e) => ontoggleinspection(e, id) }><FontAwesomeIcon icon={faInfoCircle}/></button>
+        </section>
+        <section className="gamecard-title">
+            <h5 className="game-name">{name}</h5>
+            {(yearpublished !== null) 
+                ? <h6 className="game-yearpublished">({yearpublished})</h6>
+                : <h6 className="game-yearpublished">(#{id})</h6>
+            }
+        </section>
+        <section 
+            className="gamecard-visual"
+            data-attrtype="title"
+            data-attrname={name}
+            data-newvote="thumbsup"
+            onClick={this.props.onnewvote}
+            >
+            <Thumbnail name={name} url={thumbnail} allthumbs={allthumbs} thumbcounts={thumbcounts} onnewvote={onnewvote} />
 
-        const { id, name, yearpublished, description, inspectingsection, comments, videos, allthumbs, ontoggleinspection, oninspectionsectionchange, onnewvote, ondelete, thumbnail, thumbcounts } = this.props
-        return (
-            <React.Fragment>
-            <section className="gamecard-header">
-                <button className="fa fa-button" onClick={ (e) => ondelete(e, id) }><FontAwesomeIcon icon={faTrash}/></button>
-                <button className="fa fa-button inspect" onClick={ (e) => ontoggleinspection(e, id) }><FontAwesomeIcon icon={faInfoCircle}/></button>
-            </section>
-            <section className="gamecard-collapse-control">
-                <button className="fa fa-button inspect" onClick={ (e) => ontoggleinspection(e, id) }><FontAwesomeIcon icon={faInfoCircle}/></button>
-            </section>
-            <section className="gamecard-title">
-                <h5 className="game-name">{name}</h5>
-                {(yearpublished !== null) 
-                    ? <h6 className="game-yearpublished">({yearpublished})</h6>
-                    : <h6 className="game-yearpublished">(#{id})</h6>
+        </section>
+        <ul id="inspectionsection-selector">
+            <li id="select-description" className={"segmentedcontrol darkbg" + (inspectingsection === "description" ? " selected" : "")} onClick={oninspectionsectionchange}>{this.props.reallynarrow ? 'Desc.' : 'Description'}</li>
+            <li id="select-comments" className={"segmentedcontrol darkbg" + (inspectingsection === "comments" ? " selected" : "")} onClick={oninspectionsectionchange}>Comments</li>
+            <li id="select-videos" className={"segmentedcontrol darkbg" + (inspectingsection === "videos" ? " selected" : "")} onClick={oninspectionsectionchange}>Videos</li>
+        </ul>
+        <section className="gamecard-variable">
+            <TransitionGroup>
+                {inspectingsection === "description" &&
+                    <CSSTransition 
+                        key={0}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <Description description={description} />
+                    </CSSTransition>
                 }
-            </section>
-            <section 
-                className="gamecard-visual"
-                data-attrtype="title"
-                data-attrname={name}
-                data-newvote="thumbsup"
-                onClick={this.props.onnewvote}
-                >
-                <Thumbnail name={name} url={thumbnail} allthumbs={allthumbs} thumbcounts={thumbcounts} onnewvote={onnewvote} />
-
-            </section>
-            <ul id="inspectionsection-selector">
-                <li id="select-description" className={"segmentedcontrol darkbg" + (inspectingsection === "description" ? " selected" : "")} onClick={oninspectionsectionchange}>{this.props.reallynarrow ? 'Desc.' : 'Description'}</li>
-                <li id="select-comments" className={"segmentedcontrol darkbg" + (inspectingsection === "comments" ? " selected" : "")} onClick={oninspectionsectionchange}>Comments</li>
-                <li id="select-videos" className={"segmentedcontrol darkbg" + (inspectingsection === "videos" ? " selected" : "")} onClick={oninspectionsectionchange}>Videos</li>
-            </ul>
-            <section className="gamecard-variable">
-                <TransitionGroup>
-                    {inspectingsection === "description" &&
-                        <CSSTransition 
-                            key={0}
-                            in={true}
-                            appear={false}
-                            timeout={2000}
-                            classNames={"showsegment"}
-                        >
-                            <Description description={description} />
-                        </CSSTransition>
-                    }
-                    {inspectingsection === "comments" &&
-                        <CSSTransition 
-                            key={1}
-                            in={true}
-                            appear={false}
-                            timeout={2000}
-                            classNames={"showsegment"}
-                        >
-                            <Comments comments={comments} />
-                        </CSSTransition>
-                    }
-                    {inspectingsection === "videos" &&
-                        <CSSTransition 
-                            key={2}
-                            in={true}
-                            appear={false}
-                            timeout={2000}
-                            classNames={"showsegment"}
-                        >
-                            <Videos videos={videos} />
-                        </CSSTransition>
-                    }
-                </TransitionGroup>
-            </section>
-            <section className="gamecard-footer">
-                <GameFooter gameid={id}/>
-            </section>
-            </React.Fragment>
-        )
-    }
+                {inspectingsection === "comments" &&
+                    <CSSTransition 
+                        key={1}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <Comments comments={comments} />
+                    </CSSTransition>
+                }
+                {inspectingsection === "videos" &&
+                    <CSSTransition 
+                        key={2}
+                        in={true}
+                        appear={false}
+                        timeout={2000}
+                        classNames={"showsegment"}
+                    >
+                        <Videos videos={videos} />
+                    </CSSTransition>
+                }
+            </TransitionGroup>
+        </section>
+        <section className="gamecard-footer">
+            <GameFooter gameid={id}/>
+        </section>
+        </React.Fragment>
+    )
 }
 
 GameCardBack.propTypes = {

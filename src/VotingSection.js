9507e16b3476e7a44e2e48c98621269b9f8f5d06
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-06-26 23:15:56 -0700

    Refactors the VotingSection component to be an SFC

--- src/VotingSection.js
+++ src/VotingSection.js
@@ -2,53 +2,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { VotableElement } from './VotableElement'
 
-export class VotingSection extends React.Component {
+export const VotingSection = (props) => {
 
-    render() {
-        let origVotables = JSON.parse(JSON.stringify(this.props.counts))
-        let orderedVotables = JSON.parse(JSON.stringify(this.props.counts))
-        // alphabetization is a 2nd-level sort, after the 1st sorting is by count 
-        if (this.props.alphabetize) {
+    let origVotables = JSON.parse(JSON.stringify(props.counts))
+    let orderedVotables = JSON.parse(JSON.stringify(props.counts))
+    // alphabetization is a 2nd-level sort, after the 1st sorting is by count 
+    if (props.alphabetize) {
 
-            orderedVotables = origVotables
-                .sort(function(a,b) {
+        orderedVotables = origVotables
+            .sort(function(a,b) {
 
-                    // 1st sort is by counts, 
-                    // and here the counts are different
-                    if (a.attrCount < b.attrCount) {
-                        return 1
-                    } else if (a.attrCount > b.attrCount) {
-                        return -1
+                // 1st sort is by counts, 
+                // and here the counts are different
+                if (a.attrCount < b.attrCount) {
+                    return 1
+                } else if (a.attrCount > b.attrCount) {
+                    return -1
 
-                    // 2nd sort is by names
-                    } else if (a.attrName < b.attrName) {
-                        return -1
-                    } else if (a.attrName > b.attrName) {
-                        return 1
+                // 2nd sort is by names
+                } else if (a.attrName < b.attrName) {
+                    return -1
+                } else if (a.attrName > b.attrName) {
+                    return 1
 
-                    // both count and names are equal
-                    } else {
-                        return 0
-                    }
+                // both count and names are equal
+                } else {
+                    return 0
+                }
 
-                })
-        }
-        return (
-            <ul id={this.props.elementid}>
-                <li><b>{this.props.title}</b></li>
-                {orderedVotables.map((key, index) => {
-                    return <VotableElement 
-                        key={key.attrName}
-                        preferences={this.props.sectionthumbs}
-                        attrtype={this.props.type}
-                        attrname={key.attrName} 
-                        attrcount={key.attrCount} 
-                        suppresslowcounts={this.props.suppresslowcounts}
-                        onnewvote={this.props.onnewvote}/>
-                })}
-            </ul>
-        )
+            })
     }
+    return (
+        <ul id={props.elementid}>
+            <li><b>{props.title}</b></li>
+            {orderedVotables.map((key, index) => {
+                return <VotableElement 
+                    key={key.attrName}
+                    preferences={props.sectionthumbs}
+                    attrtype={props.type}
+                    attrname={key.attrName} 
+                    attrcount={key.attrCount} 
+                    suppresslowcounts={props.suppresslowcounts}
+                    onnewvote={props.onnewvote}/>
+                })}
+        </ul>
+    )
 }
 
 VotingSection.propTypes = {

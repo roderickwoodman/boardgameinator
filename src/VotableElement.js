41a53276c81d7f5cb1713e790c14146260181c25
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-07-06 23:06:10 -0700

    Refactors the VoteableElement component as a function component

--- src/VotableElement.js
+++ src/VotableElement.js
@@ -4,53 +4,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 
 
-export class VotableElement extends React.Component {
+export const VotableElement = (props) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            hover: false
-        }        
-        this.toggleHover = this.toggleHover.bind(this)
+    let elementStyle = "voteable"
+    let vote = (props.preferences.hasOwnProperty(props.attrname)) ? 'thumbsup' : 'novote'
+    elementStyle += ' ' + vote
+    let votable_text = props.attrname
+    if (props.attrcount > 1 || !props.suppresslowcounts) {
+        votable_text += ' ('+props.attrcount+')'
     }
-
-    toggleHover() {
-        this.setState({hover: !this.state.hover})
-    }
-    
-    render() {
-        let elementStyle = (this.state.hover) ? "voteable hovering" : "voteable nothovering"
-        //FIXME, extract vote value from props: this.props.preferences['attrVote']
-        let vote = (this.props.preferences.hasOwnProperty(this.props.attrname)) ? 'thumbsup' : 'novote'
-        elementStyle += ' ' + vote
-        let votable_text = this.props.attrname
-        if (this.props.attrcount > 1 || !this.props.suppresslowcounts) {
-            votable_text += ' ('+this.props.attrcount+')'
-        }
-        return (
-            <li 
-                key={this.props.attrname} 
-                className={elementStyle} 
-                onMouseEnter={this.toggleHover} 
-                onMouseLeave={this.toggleHover}
-            > 
-                <div className="vote">
-                    { vote === 'thumbsup' &&
-                    <FontAwesomeIcon icon={faThumbsUp} />
-                    }
-                </div>
-                <div
-                    className="clickable"
-                    data-attrtype={this.props.attrtype}
-                    data-attrname={this.props.attrname}
-                    data-newvote='thumbsup'
-                    onClick={this.props.onnewvote}
-                >
-                    {votable_text}
-                </div>
-            </li>
-        )
-    }
+    return (
+        <li 
+            key={props.attrname} 
+            className={elementStyle} 
+        > 
+            <div className="vote">
+                { vote === 'thumbsup' &&
+                <FontAwesomeIcon icon={faThumbsUp} />
+                }
+            </div>
+            <div
+                className="clickable"
+                data-attrtype={props.attrtype}
+                data-attrname={props.attrname}
+                data-newvote='thumbsup'
+                onClick={props.onnewvote}
+            >
+                {votable_text}
+            </div>
+        </li>
+    )
 }
 
 VotableElement.propTypes = {

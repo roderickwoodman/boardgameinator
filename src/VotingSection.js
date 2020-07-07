ffb6389bfc602e2979f06b4d0106a3b0c3bb7e3a
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-07-06 23:10:45 -0700

    Refactors the VotableElement component to no longer be its own file

--- src/VotingSection.js
+++ src/VotingSection.js
@@ -1,6 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { VotableElement } from './VotableElement'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+
+const VotableElement = (props) => {
+
+    let elementStyle = "voteable"
+    let vote = (props.preferences.hasOwnProperty(props.attrname)) ? 'thumbsup' : 'novote'
+    elementStyle += ' ' + vote
+    let votable_text = props.attrname
+    if (props.attrcount > 1 || !props.suppresslowcounts) {
+        votable_text += ' ('+props.attrcount+')'
+    }
+    return (
+        <li 
+            key={props.attrname} 
+            className={elementStyle} 
+        > 
+            <div className="vote">
+                { vote === 'thumbsup' &&
+                <FontAwesomeIcon icon={faThumbsUp} />
+                }
+            </div>
+            <div
+                className="clickable"
+                data-attrtype={props.attrtype}
+                data-attrname={props.attrname}
+                data-newvote='thumbsup'
+                onClick={props.onnewvote}
+            >
+                {votable_text}
+            </div>
+        </li>
+    )
+}
 
 export const VotingSection = (props) => {
 

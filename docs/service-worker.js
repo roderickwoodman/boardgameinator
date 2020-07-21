c81bd5da6ca6d5426ad8e6daa59c2139a425dde5
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-07-20 23:04:34 -0700

    Deploys production code

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boardgameinator/precache-manifest.ed14f87c222263c5d49d23d7b24d5808.js"
+  "/boardgameinator/precache-manifest.9080ac8f97ed44d14b566a6b950c51eb.js"
 );
 
 self.addEventListener('message', (event) => {

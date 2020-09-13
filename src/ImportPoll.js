1caec9f4b06bd246b827c9eb76479bea709ac8ca
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-09-12 23:47:32 -0700

    Adds hardcoded poll data

--- src/ImportPoll.js
+++ src/ImportPoll.js
@@ -1,10 +1,57 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
+const hardcoded_polls = [
+    {
+        id: '1',
+        name: 'Poll A',
+        pollThumbs: {
+            titles: {
+                'Coloretto (2003)': {
+                    'Charles': 'thumbsup',
+                    'Cassidy': 'thumbsup',
+                },
+                'Wingspan': {
+                    'Wendy': 'thumbsup',
+                }
+            },
+            total_title_votes: 3
+        }
+    },
+    {
+        id: '2',
+        name: 'Poll B',
+        pollThumbs: {
+            titles: {
+                'Food Chain Magnate': {
+                    'Fran': 'thumbsup',
+                    'Filipe': 'thumbsup',
+                    'Fiona': 'thumbsup',
+                },
+                'Wingspan': {
+                    'Wesley': 'thumbsup',
+                }
+            },
+            total_title_votes: 4
+        }
+    }
+]
+
 export const ImportPoll = (props) => {
 
     return (
+        <React.Fragment>
         <h4>Import poll:</h4>
+        <ul id="import-poll">
+        { hardcoded_polls
+            .map( (poll,i) => {
+                return (
+                    <li>{poll.name}</li>
+                )
+            })
+        }
+        </ul>
+        </React.Fragment>
     )
 }
 

261060e655694b09b70d783edf5ece29ec2b9b1c
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-10-18 19:45:24 -0700

    Refactors game adding, accounting for incorrect user-supplied year

--- src/GameLibrary.js
+++ src/GameLibrary.js
@@ -1,3 +1,11 @@
+const withoutYear = (title) => {
+    if (typeof title === 'string' && title.length) {
+        return title.replace(/(( +)\(([-#]?)\d{1,6}\))$/, '')
+    } else {
+        return title
+    }
+}
+
 export const makeAllGamesActive = (cachedgametitles, game_titles) => {
 
     let status = {
@@ -11,12 +19,20 @@ export const makeAllGamesActive = (cachedgametitles, game_titles) => {
     let all_cached_ids = Object.entries(cachedgametitles).map( cachedgame => parseInt(cachedgame[1].id) )
     game_titles.forEach(function(user_title) {
 
-        // user_title was found in the cache
+        // user_title was found verbatim in the cache
         if (all_cached_disambiguous_titles.includes(user_title)) {
             if (cachedgametitles[user_title].active) {
                 status.already_active.push(user_title)
             } else { 
                 status.ready_to_make_active.push(user_title)
+            }
+
+        // user_title without the year disambiguation was found in the cache
+        } else if (all_cached_disambiguous_titles.includes(withoutYear(user_title))) {
+            if (cachedgametitles[withoutYear(user_title)].active) {
+                status.already_active.push(withoutYear(user_title))
+            } else { 
+                status.ready_to_make_active.push(withoutYear(user_title))
             }
 
         // user_title is actually an ID that was found in the cache 

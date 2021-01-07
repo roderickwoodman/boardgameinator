514e58f159c21682bd94f10d263366efe4197a06
Rod Woodman <roderickwoodman@users.noreply.github.com>
2021-01-06 22:29:00 -0800

    Refactors title vote counting to allow counting when not logged in

--- src/GameList.js
+++ src/GameList.js
@@ -198,7 +198,21 @@ export const GameList = (props) => {
                   && props.activethumbs.titles.hasOwnProperty(game.id.toString())
                   && props.activethumbs.titles[game.id].hasOwnProperty('thumbsup')) {
                     new_vote_counts.titles = props.activethumbs.titles[game.id.toString()].thumbsup.length
-                    const myVote = JSON.parse(JSON.stringify(props.activethumbs.titles[game.id.toString()].thumbsup)).filter( vote => vote.user === props.user )
+                    const myVote = JSON.parse(JSON.stringify(props.activethumbs.titles[game.id.toString()].thumbsup)).filter( (vote) => {
+                        if (props.user !== null) {
+                            if (vote.user === props.user) {
+                                return true
+                            } else {
+                                return false
+                            }
+                        } else {
+                            if (vote.user === null) {
+                                return true
+                            } else {
+                                return false
+                            }
+                        }
+                    })
                     if (myVote.length === 1) {
                         new_vote_counts.my_rank = myVote.rank
                     } else {

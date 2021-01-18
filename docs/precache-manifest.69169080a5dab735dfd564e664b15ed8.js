bfc1a0d630bcb3d7c993c89a4aa21663cdf9b5d1
Rod Woodman <roderickwoodman@users.noreply.github.com>
2021-01-17 23:18:41 -0800

    Deploys production code

--- docs/precache-manifest.69169080a5dab735dfd564e664b15ed8.js
+++ docs/precache-manifest.69169080a5dab735dfd564e664b15ed8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a807f86498a71d9a79559f2999526ea",
+    "revision": "88474fbe14410d8a57ec5895d5fc3570",
     "url": "/boardgameinator/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/boardgameinator/static/css/2.86aa6515.chunk.css"
   },
   {
-    "revision": "4f65f0ae5006bc79db5b",
-    "url": "/boardgameinator/static/css/main.da15ba8f.chunk.css"
+    "revision": "e5f708eed666cfc25567",
+    "url": "/boardgameinator/static/css/main.1b4260db.chunk.css"
   },
   {
     "revision": "49c265442d7f541a3d5c",
     "url": "/boardgameinator/static/js/2.d3cb6682.chunk.js"
   },
   {
-    "revision": "4f65f0ae5006bc79db5b",
-    "url": "/boardgameinator/static/js/main.90c00e1c.chunk.js"
+    "revision": "e5f708eed666cfc25567",
+    "url": "/boardgameinator/static/js/main.c1198b96.chunk.js"
   },
   {
     "revision": "670d3fa99576bb756c63",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boardgameinator/precache-manifest.6f13010c878b4715300c1a1f6b515e0d.js"
+  "/boardgameinator/precache-manifest.69169080a5dab735dfd564e664b15ed8.js"
 );
 
 self.addEventListener('message', (event) => {
